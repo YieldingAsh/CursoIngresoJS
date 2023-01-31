a8066734708654f046c862896c94aae00b8b8c68
YieldingAsh <matiruli2016@gmail.com>
2023-01-31 16:58:18 -0300

    Actulizado 31/1/2023

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let valor1 =  document.getElementById("txtIdImporte").value;
+	let total = ((80 * valor1) / 100);
+	total = parseInt(total);
+	document.getElementById("txtIdResultado").value = total;
 }
