81895fe50dd01f5a2552f4f74ef4f1eccda3de97
YieldingAsh <matiruli2016@gmail.com>
2023-01-31 16:22:46 -0300

    Actualizado 31/1/2023

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,42 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let valor1 =  document.getElementById("txtIdNumeroUno").value;
+	let valor2 = document.getElementById("txtIdNumeroDos").value;
+	valor1 = parseInt(valor1);
+	valor2 = parseInt(valor2);
+	let total = valor1 + valor2
+	alert("La suma es " + total);	
 }
 
 function restar()
 {
-	
+	let valor1 =  document.getElementById("txtIdNumeroUno").value;
+	let valor2 = document.getElementById("txtIdNumeroDos").value;
+	valor1 = parseInt(valor1);
+	valor2 = parseInt(valor2);
+	let total = valor1 - valor2
+	alert("La resta es " + total);
 }
 
 function multiplicar()
 { 
-	
+	let valor1 =  document.getElementById("txtIdNumeroUno").value;
+	let valor2 = document.getElementById("txtIdNumeroDos").value;
+	valor1 = parseInt(valor1);
+	valor2 = parseInt(valor2);
+	let total = valor1 * valor2
+	alert("La multiplicacion es " + total);
 }
 
 function dividir()
 {
-	
+	let valor1 =  document.getElementById("txtIdNumeroUno").value;
+	let valor2 = document.getElementById("txtIdNumeroDos").value;
+	valor1 = parseInt(valor1);
+	valor2 = parseInt(valor2);
+	let total = valor1 / valor2;
+	total = parseInt(total);
+	alert("La divicion es " + total);
 }
 
