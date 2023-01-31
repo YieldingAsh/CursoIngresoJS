cd0abce686eaf0a5211eb694ea1212a76ae74773
YieldingAsh <matiruli2016@gmail.com>
2023-01-31 16:51:45 -0300

    Actualizado 31/1/2023

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,8 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
+	let valor1 =  document.getElementById("txtIdSueldo").value;
+	let total = ((110 * valor1) / 100);
+	total = parseInt(total);
+	document.getElementById("txtIdResultado").value = total;
 }
