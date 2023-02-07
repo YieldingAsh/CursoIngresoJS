34f2aaeca6ce31d8b5c1f2ae5a483b04927d2b7f
YieldingAsh <matiruli2016@gmail.com>
2023-02-07 14:52:14 -0300

    actulizado 7/2/2023

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,9 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
-
-
+	let edad = document.getElementById("txtIdEdad").value;
+	let estado = document.getElementById("estadoCivil").value;
+	if(edad >= 18 && estado == "soltero")
+	{
+		alert("Es soltero y no es menor.")
+	}
 }//FIN DE LA FUNCIÓN
