eb8d4e35a13a9722d3e5c96494a848ca11d735e2
YieldingAsh <matiruli2016@gmail.com>
2023-02-07 15:01:44 -0300

    actualizado 7/2/2023

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
-
-}//FIN DE LA FUNCIÓN
+	let n = Math.floor(Math.random() * 10)
+	if(n <= 4)
+	{
+		alert("Vamos, la proxima se puede")
+	}
+	else if(n > 4)
+	{
+		if(n <= 8)
+		{
+			alert("APROBÓ")
+		}
+		else
+		{
+			alert("EXCELENTE")
+		}
+	}
+}
