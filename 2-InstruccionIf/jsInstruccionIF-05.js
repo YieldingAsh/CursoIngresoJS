a11abcf8fc683a7b304290e226d0d606a14f8e4b
YieldingAsh <matiruli2016@gmail.com>
2023-02-07 14:38:26 -0300

    actualizado 7/2/2023

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,8 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-
+	let edad = document.getElementById("txtIdEdad").value;
+	if(edad <= 13 || edad >= 17)
+	{
+		alert("Usted no es un adolecente")
+	}
 }//FIN DE LA FUNCIÓN
