62cdef03ba8aa9b3e2c0503f3744737ed481c0ae
YieldingAsh <matiruli2016@gmail.com>
2023-02-07 18:28:08 -0300

    actulizado 7/2/2023

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,28 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
 function Sumar () 
 {
-	
+    let precio1 =  parseFloat(document.getElementById("txtIdPrecioUno").value);
+	let precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+	let total = precio1 + precio2 + precio3;
+	alert("La suma total de los precios es " + total);
 }
 function Promedio () 
 {
-	
+    let precio1 =  parseFloat(document.getElementById("txtIdPrecioUno").value);
+	let precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+	let total = (precio1 + precio2 + precio3) / 3;
+	alert("El promedio total de los precios es " + total);
 }
 function PrecioFinal () 
 {
-	
+    let precio1 =  parseFloat(document.getElementById("txtIdPrecioUno").value);
+	let precio2 = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    let precio3 = parseFloat(document.getElementById("txtIdPrecioTres").value);
+	let total = ((121 * (precio1 + precio2 + precio3)) / 100);
+	alert("El promedio total de los precios es " + total);
 }
