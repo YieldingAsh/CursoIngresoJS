df29b390a2c71b0cab8ca4f080cffd99d92f8f31
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 16:00:48 -0300

    actulizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -2,6 +2,20 @@ function mostrar()
 {
 	//tomo la hora
 	var horaDelDia =txtIdHora.value;
+	switch(horaDelDia)
+	{
+		case ("7"):
+		case ("8"):
+		case ("9"):
+		case ("10"):
+		case ("11"):
+			horaDelDia = "Es de mañana.";
+			break;
+		default:
+			horaDelDia = "No es de mañana."
+			break;
+	}
+
 	alert(horaDelDia);
 	
 	
