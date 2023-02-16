b95e88cbcdfb00a37617cea75e11b25fdf673f49
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 16:19:06 -0300

    actualizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,20 @@
-function mostrar()
-{
-	var destinoIngresado =txtIdDestino.value;
+function mostrar() {
+	var destinoIngresado = txtIdDestino.value;
+	switch (destinoIngresado) 
+	{
+		case ("Bariloche"):
+			destinoIngresado = "Frio";
+			break;
+		case ("Mar de plata"):
+			destinoIngresado = "Calor";
+			break;
+		case ("Cataratas"):
+			destinoIngresado = "Frio";
+			break;
+		case ("Ushuaia"):
+			destinoIngresado = "Frio";
+			break;
+	}
 	alert(destinoIngresado);
 
 }//FIN DE LA FUNCIÓN
