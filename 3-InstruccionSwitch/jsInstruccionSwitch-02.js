aaae7bd0bc701822c12ee9838caad7e1b5180359
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 15:43:44 -0300

    actualizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -2,8 +2,48 @@ function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-
+	var mensaje 
+	switch(mesDelAño)
+	{
+		case("Enero"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Febrero"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Marzo"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Abril"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Mayo"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Junio"):
+			mensaje = "Falta para el invierno.";
+			break;
+		case("Julio"):
+			mensaje = "Abrigate que hace frio.";
+			break;
+		case("Agosto"):
+			mensaje = "Abrigate que hace frio.";
+			break;
+		case("Septiembre"):
+			mensaje = "Ya pasamos el frio, ahora calor!!!.";
+			break;
+		case("Octubre"):
+			mensaje = "Ya pasamos el frio, ahora calor!!!.";
+			break;
+		case("Noviembre"):
+			mensaje = "Ya pasamos el frio, ahora calor!!!.";
+			break;
+		case("Diciembre"):
+			mensaje = "Ya pasamos el frio, ahora calor!!!.";
+			break;
+	}
+	
+	alert(mensaje);
 
 
 
