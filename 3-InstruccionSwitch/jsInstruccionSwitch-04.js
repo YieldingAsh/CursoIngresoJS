dafe8506a8488f4d88cc576654df86a1bd06d4ab
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 15:56:21 -0300

    actulizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -2,9 +2,27 @@ function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mensaje 
+	switch(mesDelAño)
+	{
+		case("Enero"):
+		case("Marzo"):
+		case("Mayo"):
+		case("Julio"):
+		case("Agosto"):
+		case("Octubre"):
+		case("Diciembre"):
+			mensaje = "si tiene 31 días.";
+			break;
+		case("Febrero"):
+			mensaje = "si tiene 28 días.";
+			break;
+		default:
+			mensaje = "si tiene 30 días.";
+			break;
+	}
 	
-	
+	alert(mensaje);
 
 
 
