fef6ccdf0a1b33d56868992cf23f1809c952db07
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 15:29:40 -0300

    actualizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -2,7 +2,25 @@ function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mensaje 
+	switch(mesDelAño)
+	{
+		case("Enero"):
+			mensaje = "que comiences bien el año!!!.";
+			break;
+		case("Marzo"):
+			mensaje = "a clases!!!.";
+			break;
+		case("Julio"):
+			mensaje = "se vienen las vacaciones!!!.";
+			break;
+		case("Diciembre"):
+			mensaje = "Felices fiesta!!!."
+			break;
+
+	}
+	
+	alert(mensaje);
 
 
 
