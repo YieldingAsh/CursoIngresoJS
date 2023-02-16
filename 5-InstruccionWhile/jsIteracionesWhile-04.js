8f9c55371df1badb2f94d2e55fd45668f72d14a9
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 18:10:18 -0300

    actualizado 16/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -4,6 +4,11 @@ pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
 	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
+	numeroIngresado = prompt("ingrese un número entre 0 y 9.");
+	while(parseInt(numeroIngresado) < 0 || parseInt(numeroIngresado) > 9)
+	{
+		alert ("Ingresar un numero entre 0 y 9 porfavor")
+		numeroIngresado = prompt("ingrese un número entre 0 y 9.");
+	}
+	document.getElementById("txtIdNumero").value = parseInt(numeroIngresado);
 }//FIN DE LA FUNCIÓN
