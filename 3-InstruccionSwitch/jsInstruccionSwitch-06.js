6c545f01a078f31c0d387891c83f2ffb67553052
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 16:06:19 -0300

    actualizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -1,7 +1,44 @@
-function mostrar()
+function mostrar() 
 {
 	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
+	var horaDelDia = txtIdHora.value;
+	switch (horaDelDia) 
+	{
+		case ("7"):
+		case ("8"):
+		case ("9"):
+		case ("10"):
+		case ("11"):
+			horaDelDia = "Es de mañana.";
+			break;
+		case ("12"):
+		case ("13"):
+		case ("14"):
+		case ("15"):
+		case ("16"):
+		case ("17"):
+		case ("18"):
+		case ("19"):
+			horaDelDia = "Es de tarde.";
+			break;
+		case ("20"):
+		case ("21"):
+		case ("22"):
+		case ("23"):
+		case ("24"):
+		case ("0"):
+		case ("1"):
+		case ("2"):
+		case ("3"):
+		case ("4"):
+		case ("5"):
+		case ("6"):
+			horaDelDia = "Es de noche.";
+			break;
+		default:
+			horaDelDia = "La hora no existe."
+			break;
+	}
 
-}//FIN DE LA FUNCIÓN
+	alert(horaDelDia);
+}
