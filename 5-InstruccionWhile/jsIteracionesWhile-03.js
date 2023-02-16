cab0913b84bf704179dd0fbdbd5b75a60ab8c4f4
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 17:55:45 -0300

    actualizado 16/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -4,5 +4,11 @@ function mostrar()
 {
 	var claveIngresada;
 	claveIngresada = prompt("ingrese el número clave.");
+	while(claveIngresada != "utn750")
+	{
+		alert ("clave incorrecta intente de vuelta")
+		break;
+	}
+	claveIngresada = prompt("ingrese el número clave.");
 	
 }//FIN DE LA FUNCIÓN
