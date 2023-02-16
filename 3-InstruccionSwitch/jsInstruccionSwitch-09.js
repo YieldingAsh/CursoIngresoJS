c839de85703d1576fee55f89b173cdd3ec121344
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 17:05:32 -0300

    actualizado 16/2/2023

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,69 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+	var estacionIngresada = txtIdEstacion.value;
+	var destino = txtIdDestino.value;
+	var aumento = 0
+	var descuento = 0
+	if (estacionIngresada = "Invierno") 
+	{
+		switch (destino) 
+		{
+			case ("Bariloche"):
+				aumento = 20;
+				break;
+			case ("Mar de plata"):
+				descuento = 20;
+				break;
+			default:
+				descuento = 10;
+				break;
+		}
+	}
+	else if (estacionIngresada = "Verano")
+	{
+		switch (destino) 
+		{
+			case ("Bariloche"):
+				descuento = 20;
+				break;
+			case ("Mar de plata"):
+				aumento = 20;
+				break;
+			default:
+				aumento = 10;
+				break;
+		}
+	}
+	else
+	{
+		switch (destino) 
+		{
+			case ("Bariloche"):
+				aumento = 10;
+				break;
+			case ("Mar de plata"):
+				aumento = 10;
+				break;
+			case ("Cataratas"):
+				aumento = 10;
+				break;
+			default:
+				descuento = 0;
+				break;
+		}
+	}
 
+	if(aumento > 0)
+	{
+		let total = (((aumento + 100 ) * 15000) / 100);
+		alert ("Precio Final es " + total);
+	}
+	else if (descuento > 0)
+	{
+		let total = (((100 - descuento)  * 15000) / 100);
+		alert ("Precio Final es " + total); 
+	}
+	else
+	{
+		alert("Precio Final es 15.000")
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,47 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
+function mostrar() {
+	var estacionIngresada = txtIdEstacion.value;
+	var destino = txtIdDestino.value;
+	if (estacionIngresada = "Invierno") 
+	{
+		switch (destino) 
+		{
+			case ("Bariloche"):
+				estacionIngresada = "se viaja";
+				break;
+			default:
+				estacionIngresada = "No se viaja";
+				break;
+		}
+	}
+	else if (estacionIngresada = "Verano")
+	{
+		switch (destino) 
+		{
+			case ("Mar de plata"):
+			case ("Cataratas"):
+				estacionIngresada = "se viaja";
+				break;
+			default:
+				estacionIngresada = "No se viaja";
+				break;
+		}
+	}
+	else if(estacionIngresada = "Primavera")
+	{
+		switch(destino)
+		{
+			case("Bariloche"):
+				estacionIngresada = "No se viaja";
+				break;
+			default:
+				estacionIngresada = "Se viaja";
+				break;
+		}
+	}
+	else
+	{
+		estacionIngresada = "Se viaja";
+	}
 	alert(estacionIngresada);
 
 }//FIN DE LA FUNCIÓN
