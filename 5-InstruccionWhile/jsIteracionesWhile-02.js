7948a4fd0471b2df2818674d0cb1c429f56ac4ff
YieldingAsh <matiruli2016@gmail.com>
2023-02-16 17:47:41 -0300

    actualizado 16/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,8 +3,12 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
-
+	let i = 0
+	let n = 11
+	while( i <= 9)
+	{
+		i++;
+		n--;
+		alert(n);
+	}
 }//FIN DE LA FUNCIÓN
