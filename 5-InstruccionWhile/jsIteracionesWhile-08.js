3a521b5c7d1916c06f506ba17eb2e816563a833f
YieldingAsh <matiruli2016@gmail.com>
2023-02-26 14:56:25 -0300

    Actualizado 26/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,15 +3,41 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
+	let contador;
+	let parar;
+	let sumaPositivos;
+	let multiplicacionNegativos;
 	contador=0;
 	sumaPositivos=0;
 	multiplicacionNegativos=1;
-	respuesta='si';
+	parar="NO";
+	while (parar != "SI")
+	{
+		contador++;
+		numeroIngresado = prompt("Ingrese el numero" + contador + " (cuando desee ver el resultado ponga SI)");
+		if (numeroIngresado == "SI")
+		{
+			contador--;
+			parar = "SI";
+			alert("Informando cuenta");
+			break;
+		}
 
+		numeroIngresado = parseInt(numeroIngresado);
+		if(numeroIngresado < 0)
+		{
+			multiplicacionNegativos = multiplicacionNegativos * numeroIngresado;
+		}
+		else if (numeroIngresado >= 0)
+		{
+			sumaPositivos = sumaPositivos + numeroIngresado;
+		}
+		else
+		{
+			alert("ingrese un numero valido porfavor");
+			contador--;
+		}
+	}
 
 	txtIdSuma.value=sumaPositivos;
 	txtIdProducto.value=multiplicacionNegativos;
