bbb8eb9319de2ab26078aa45d30f602bf4f17abe
YieldingAsh <matiruli2016@gmail.com>
2023-02-26 14:30:36 -0300

    Actualizado 26/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -8,10 +8,17 @@ function mostrar()
 	while (contador <= 4)
 	{
 		contador++;
-		numeroIngresado =  parseInt(prompt("Ingrese el numero" + contador));
-		acumulador = acumulador + numeroIngresado;
+		numeroIngresado = parseInt(prompt("Ingrese el numero" + contador));
+		if(numeroIngresado <= 0 || numeroIngresado >= 0)
+		{
+			acumulador = acumulador + numeroIngresado;
+		}
+		else
+		{
+			alert("ingrese un numero valido porfavor");
+			contador--;
+		}
 	}
-	
 	txtIdSuma.value=acumulador;
 	txtIdPromedio.value=acumulador/5;
 }//FIN DE LA FUNCIÓN
