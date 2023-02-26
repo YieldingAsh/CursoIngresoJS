2c23dedeab7939196f2d1d8b487e89b15ae88c7d
YieldingAsh <matiruli2016@gmail.com>
2023-02-26 15:56:24 -0300

    Actulizado 26/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -2,20 +2,62 @@
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
+{	// declarar letiables
+	let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo;
+	let numeroMinimo;
+	let parar;
+	let contador;
 	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+	contador = 0;
+	banderaDelPrimero = "es el primero";
+	parar = "NO";
+	while (parar != "SI")
 	{
-		
-		respuesta=prompt("desea continuar?");
+		contador++;
+		numeroIngresado = prompt("Ingrese el numero" + contador + " (cuando desee ver el resultado ponga SI)");
+		if (numeroIngresado == "SI")
+		{
+			contador--;
+			parar = "SI";
+			alert("Mostrando informacion");
+			break;
+		}
+
+		numeroIngresado = parseInt(numeroIngresado);
+		while(banderaDelPrimero == "es el primero")
+		{
+			if(numeroIngresado <= 0 || numeroIngresado >= 0)
+			{
+				numeroMaximo = numeroIngresado;
+				numeroMinimo = numeroIngresado;
+				banderaDelPrimero = "no";
+			}
+			else
+			{
+				alert("ingrese un numero valido porfavor");
+				numeroIngresado = parseInt(prompt("Ingrese el numero" + contador + " (cuando desee ver el resultado ponga SI)"));
+			}
+		}
+		if(numeroIngresado <= 0 || numeroIngresado >= 0)
+		{
+			if( numeroIngresado < numeroMinimo)
+			{
+				numeroMinimo = numeroIngresado;
+			}
+			else if( numeroIngresado > numeroMaximo)
+			{
+				numeroMaximo = numeroIngresado;
+			}
+		}
+		else
+		{
+			alert("ingrese un numero valido porfavor");
+			contador--;
+		}
 	}
+	
 	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	txtIdMinimo.value=numeroMinimo;
 }//FIN DE LA FUNCIÓN
