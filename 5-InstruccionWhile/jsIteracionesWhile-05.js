c3971b5fbde318d80f92310353a6f94d7a772c5e
YieldingAsh <matiruli2016@gmail.com>
2023-02-26 13:52:36 -0300

    Actualizado 26/2/2023

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -3,10 +3,12 @@ Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
-	var sexoIngresado;
+	let sexoIngresado;
 	sexoIngresado = prompt("ingrese f ó m .");
-
-
-
+	while (sexoIngresado != "f" && sexoIngresado != "m")
+	{
+		alert("Ingrese un sexo valido porfavor, f para femenino o m para masculino");
+		sexoIngresado = prompt("ingrese f ó m .");
+	}
 	txtIdSexo.value=sexoIngresado;
 }//FIN DE LA FUNCIÓN
